88df3dbf56b76c0f72aaa2976ff9dcfe78f2277c
EndaMoore <enda.moore@student.ncilr.ie>
2015-06-11 23:14:28 +0100

    Pyramid class completed

--- Test/src/Pyramid.java
+++ Test/src/Pyramid.java
@@ -5,25 +5,26 @@ import acm.graphics.*;
 import acm.graphics.*;
 import acm.program.*;
 
-public class Pyramid {
+public class Pyramid extends GraphicsProgram{
 	
-	//public static final int WINDOW_WIDTH = 600;
-	//public static final int WINDOW_HEIGHT = 400;
+	
 	public static final int BRICK_WIDTH = 30;
-	public static final int BRICK_HEIGTH= 12;
+	public static final int BRICK_HEIGTH = 12;
 	public static final int BRICK_IN_BASE = 14;
+
 	
 	public void run() {
 		
-		//setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-		
-		GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGTH);
-		brick.setColor(Color.BLACK);
-		
-		for (int i = 0; i < 14; i++) {
-			//add(brick);
+		for (int i = 0; i < BRICK_IN_BASE; i++) {
+			for (int j = 0; j < (BRICK_IN_BASE - i); j++) {
+				int x = (getWidth() / 2) - ((BRICK_IN_BASE - i) * BRICK_WIDTH) / 2 + j * BRICK_WIDTH;
+				int y = getHeight() - ((i + 1) * BRICK_HEIGTH);
+				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGTH);
+				add(brick);
+			}
 		}
+		
+		
+		
 	}
-	
-
 }
